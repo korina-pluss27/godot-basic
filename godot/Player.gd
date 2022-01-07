172e029b11155d5960ec4444ee9fab91af52dbf7
korina-pluss27 <korina.pluss@gmail.com>
2022-01-07 04:34:25 -0700

    tutorial files :sparkles: material gorl

---
+++ godot/Player.gd
@@ -0,0 +1,59 @@
+extends Area2D
+signal hit
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+# export allows us to set the value in Inspector
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	# Check for input.
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		
+	# Play the appropriate animation.
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		# $ is shorthand for get_node()
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	# Move in the given direction, clamp to screen size
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(_body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
